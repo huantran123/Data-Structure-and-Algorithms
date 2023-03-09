3026ee856a2219bf120bb52a2774bca1b12b83a7
Huan Tran <huanductran98@gmail.com>
2023-03-08 23:35:22 -0800

    Complete insertionSort

--- algorithms/sorting/insertionSort.js
+++ algorithms/sorting/insertionSort.js
@@ -30,6 +30,7 @@ const arr = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 console.log(insertionSort(arr)) // [0, 1, 2, 4, 5, 6, 44, 63, 87, 99, 283]
 
 
+
 // Another way:
 
 // function insertionSort(array) {
