e7c353736520625b91b631708672a4c28798b9ae
Huan Tran <huanductran98@gmail.com>
2023-03-08 23:34:42 -0800

    Complete insertionSort.js

--- algorithms/sorting/README.md
+++ algorithms/sorting/README.md
@@ -1,6 +1,7 @@
 # Sorting Algorithms
 
 ## Bubble Sort
+- Repeatedly swapping the adjacent elements if they are in the wrong order
 ### Code
   [bubbleSort.js](./bubbleSort.js)
 ### Time Comlexity & Space Complexity
@@ -12,6 +13,7 @@
   - Worst: O(1)
 
 ## Selection Sort
+- Repeatedly selects the smallest (or largest) element from the unsorted portion of the list and swaps it with the first element of the unsorted portion. This process is repeated for the remaining unsorted portion of the list until the entire list is sorted.
 ### Code
   [selectionSort.js](./selectionSort.js)
 ### Time Comlexity & Space Complexity
@@ -19,5 +21,18 @@
   | Best | Average | Worst |
   |------|---------|-------|
   |O(n^2)| O(n^2)  | O(n^2)|
+#### Space Complexity
+  - Worst: O(1)
+
+## Insertion Sort
+- Repeatedly taking an element from the unsorted portion of the array and inserting it into its correct position in the sorted portion of the array.
+- This sorting is efficient when the list is almost or already sorted
+### Code
+  [insertionSort.js](./insertionSort.js)
+### Time Comlexity & Space Complexity
+#### Time Complexity
+  | Best | Average | Worst |
+  |------|---------|-------|
+  | O(n) | O(n^2)  | O(n^2)|
 #### Space Complexity
   - Worst: O(1)
---
+++ algorithms/sorting/insertionSort.js
@@ -0,0 +1,54 @@
+const insertionSort = (arr) => {
+  // Start from the beginning of the array
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < arr[0]) {
+      //move number to the first position
+      arr.unshift(arr.splice(i,1)[0]);
+    } else {
+      // Take a portion of the array from the first index to the current index,
+      // iterate backwards to compare and swap the current index to the right spot
+      for (let j = i; j > 0; j--) {
+        if (arr[j] < arr[j-1]) {
+          let temp = arr[j];
+          arr[j] = arr[j-1];
+          arr[j-1] = temp;
+      // after it goes to the right spot, whcih means that the current index now is greater that the previous
+      // break the backward interation and continue to compare the next index of the array
+        } else {
+          break;
+        }
+      }
+    }
+  }
+  return arr;
+}
+
+// Time complexity: O(n^2)
+// Space complexity: O(1)
+
+const arr = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+console.log(insertionSort(arr)) // [0, 1, 2, 4, 5, 6, 44, 63, 87, 99, 283]
+
+
+// Another way:
+
+// function insertionSort(array) {
+//   const length = array.length;
+// 	for (let i = 0; i < length; i++) {
+		// if (array[i] < array[0]) {
+    //   //move number to the first position
+    //   array.unshift(array.splice(i,1)[0]);
+    // } else {
+//       // only sort number smaller than number on the left of it. This is the part of insertion sort that makes it fast if the array is almost sorted.
+//       if (array[i] < array[i-1]) {
+//         //find where number should go
+//         for (var j = 1; j < i; j++) {
+//           if (array[i] >= array[j-1] && array[i] < array[j]) {
+//             //move number to the right spot
+//             array.splice(j,0,array.splice(i,1)[0]);
+//           }
+//         }
+//       }
+//     }
+// 	}
+// }
