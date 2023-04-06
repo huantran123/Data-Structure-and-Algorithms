538e46a13c5dc3c44c92d2ce82319369eab0f052
Huan Tran <huanductran98@gmail.com>
2023-04-06 00:09:55 -0700

    Complete mergeTwoLists

--- leetcode/linked-list/easy/mergeTwoLists.js
+++ leetcode/linked-list/easy/mergeTwoLists.js
@@ -29,3 +29,24 @@ var mergeTwoLists = function(list1, list2) {
   }
   return result;
 };
+
+// Another way using while loop:
+// var mergeTwoLists = function(list1, list2) {
+//     const result = new ListNode();
+//     let merged = result;
+
+//     while (list1 && list2) {
+//         if (list1.val <= list2.val) {
+//             merged.next = list1;
+//             list1 = list1.next
+//         } else {
+//             merged.next = list2;
+//             list2 = list2.next
+//         }
+//         merged = merged.next;
+//     }
+
+//     merged.next = list1 || list2;
+
+//     return result.next;
+// }
