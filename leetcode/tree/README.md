4ed92aa6405999fe163ee6705b93caeb8adbe2ec
Huan Tran <huanductran98@gmail.com>
2023-08-20 21:36:58 -0700

    Leetcode: Tree - recoverTree.js

--- leetcode/tree/README.md
+++ leetcode/tree/README.md
@@ -14,3 +14,4 @@
   | [Validate BST](https://leetcode.com/problems/validate-binary-search-tree/) | [isValidBST.js](./medium/isValidBST.js) |  | Recursion and compare |
   | [Kth Smallest Element in a BST](https://leetcode.com/problems/kth-smallest-element-in-a-bst/) | [kthSmallest.js](./medium/kthSmallest.js) |  | Inorder traverse the tree |
   | [Unique Binary Search Trees II](https://leetcode.com/problems/unique-binary-search-trees-ii/) | [generateUniqueBSTs.js](./medium/generateUniqueBSTs.js) |  | Recursion: recursively generate trees for left and right |
+  | [Recover Binary Search Tree](https://leetcode.com/problems/recover-binary-search-tree/) | [recoverTree.js](./medium/recoverTree.js) |  | Traverse inorder through the tree to find 2 nodes that need to be swapped |
---
+++ leetcode/tree/medium/recoverTree.js
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/recover-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var recoverTree = function(root) {
+  // array of nodes in ascending order
+  const nodes = []
+
+  // traverse inorder through the tree and push nodes to the array
+  const inorderTraverse = (node) => {
+      node.left && inorderTraverse(node.left)
+      nodes.push(node)
+      node.right && inorderTraverse(node.right)
+  }
+
+  inorderTraverse(root)
+
+  // after storing all the nodes to the array, traverse over the array
+  for (let i = 0; i < nodes.length - 1; i++) {
+      // detect if there's any place in the array where a node has value greater than the next
+      if (nodes[i].val > nodes[i + 1].val) {
+          // pointer index
+          let index = i + 1
+          // get the last index of node that has value less than the current node (nodes[i])
+          while (nodes[index] && nodes[index].val < nodes[i].val) {
+              index++
+          }
+          // while loop terminates at nodes[index] => nodes[index-1] is the node we're finding
+          // now swap the values of current node and nodes[index] then stop the for loop
+          let temp = nodes[i].val
+          nodes[i].val = nodes[index-1].val
+          nodes[index-1].val = temp
+          break
+      }
+  }
+};
